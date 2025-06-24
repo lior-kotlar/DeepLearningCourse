2653960e5e19364874b6d19fdb1c8438ac5322ea
Lior Kotlar <liorkotlar@gmail.com>
2025-06-24 21:49:04 +0300

    finished writing code for task 1. with custom reviews and everything

--- exercise3/code/loader.py
+++ exercise3/code/loader.py
@@ -26,35 +26,32 @@ def review_clean(text):
     return text.strip()
 
 
-def tokinize(s):
+def tokenize(s):
     s = review_clean(s).lower()
     splited = s.split()
     return splited[:MAX_LENGTH]
 
 
 def load_data_set(load_my_reviews=False):
+    custom_iter = None
     data=pd.read_csv("exercise3/data/IMDB Dataset.csv")
     train_data=data[:Train_size]
     train_iter=ReviewDataset(train_data["review"],train_data["sentiment"])
-    print(f'train iter: {train_iter[0]}')
-    print(f'train iter: {train_iter[1]}')
-    print(f'train iter: {train_iter[2]}')
     test_data=data[Train_size:]
     if load_my_reviews:
-        my_data = pd.DataFrame({"review": my_test_texts, "sentiment": my_test_labels})
-        pd.concat([test_data,my_data])
-        # test_data=test_data.append(my_data)
+        custom_data = pd.DataFrame({"review": my_test_texts, "sentiment": my_test_labels})
+        custom_iter = ReviewDataset(custom_data["review"],custom_data["sentiment"])
     test_data=test_data.reset_index(drop=True)
     test_iter=ReviewDataset(test_data["review"],test_data["sentiment"])
-    return train_iter, test_iter
+    return train_iter, test_iter, custom_iter
 
 
 embedding = GloVe(name='6B', dim=embedding_size)
-tokenizer = get_tokenizer(tokenizer=tokinize)
+tokenizer = get_tokenizer(tokenizer=tokenize)
 
 
 def preprocess_review(s):
-    cleaned = tokinize(s)
+    cleaned = tokenize(s)
     embedded = embedding.get_vecs_by_tokens(cleaned)
     if embedded.shape[0] != 100 or embedded.shape[1] != 100:
         embedded = torch.nn.functional.pad(embedded, (0, 0, 0, MAX_LENGTH - embedded.shape[0]))
@@ -65,13 +62,13 @@ def preprocess_label(label):
     return [0.0, 1.0] if label == "negative" else [1.0, 0.0]
 
 
-def collect_batch(batch):
+def collate_batch(batch):
     label_list = []
     review_list = []
     embedding_list=[]
-    for  review,label in batch:
+    for review,label in batch:
         label_list.append(preprocess_label(label))### label
-        review_list.append(tokinize(review))### the  actuall review
+        review_list.append(tokenize(review))### the  actuall review
         processed_review = preprocess_review(review).detach()
         embedding_list.append(processed_review) ### the embedding vectors
     label_list = torch.tensor(label_list, dtype=torch.float32).reshape((-1, 2))
@@ -84,10 +81,11 @@ def collect_batch(batch):
 ##########################
 
 my_test_texts = []
-my_test_texts.append(" this movie is very very bad ,the worst movie ")
-my_test_texts.append(" this movie is so great")
-my_test_texts.append("I really  liked the fish and animations the anther casting was not so good ")
-my_test_labels = ["neg", "pos", "pos"]
+my_test_texts.append("this movie is very very bad ,the worst movie")
+my_test_texts.append("this movie is so great")
+my_test_texts.append("I really liked the fish and animations the anther casting was not so good")
+my_test_texts.append("i am happy that we left halfway through the movie. i recommend everyone to do the same")
+my_test_labels = ["negative", "positive", "positive", "negative"]
 
 ##########################
 ##########################
@@ -110,11 +108,12 @@ class ReviewDataset(torch.utils.data.Dataset):
 
 
 def get_data_set(batch_size, toy=False):
-        train_data, test_data = load_data_set(load_my_reviews=toy)
+        train_data, test_data, custom_data = load_data_set(load_my_reviews=toy)
+
         train_dataloader = DataLoader(train_data, batch_size=batch_size,
-                                      shuffle=True, collate_fn=collect_batch)
+                                      shuffle=True, collate_fn=collate_batch)
         test_dataloader = DataLoader(test_data, batch_size=batch_size,
-                                     shuffle=True, collate_fn=collect_batch)
-        return train_dataloader, test_dataloader, MAX_LENGTH, embedding_size
-
-
+                                     shuffle=True, collate_fn=collate_batch)
+        custom_dataloader = DataLoader(custom_data, batch_size=batch_size,
+                                       shuffle=False, collate_fn=collate_batch) if toy else None
+        return train_dataloader, test_dataloader, custom_dataloader, MAX_LENGTH, embedding_size
--- exercise3/code/sentiment_start.py
+++ exercise3/code/sentiment_start.py
@@ -5,7 +5,7 @@
 ##                                                                    ##
 ########################################################################
 ########################################################################
-
+import pandas as pd
 import torch
 from torch.nn.functional import pad
 import torch.nn as nn
@@ -13,7 +13,6 @@ import numpy as np
 import loader as ld
 import matplotlib.pyplot as plt
 import os
-
 
 BATCH_SIZE = 64
 OUTPUT_SIZE = 2
@@ -23,16 +22,20 @@ MODEL_SAVE_DIRECTORY = 'exercise3/models'
 PLOT_SAVE_DIRECTORY = 'exercise3/plots'
 
 run_recurrent = True    # else run Token-wise MLP
-use_RNN = False         # otherwise GRU
+use_RNN = True         # otherwise GRU
 atten_size = 0          # atten > 0 means using restricted self atten
+SHOW_CUSTOM_REVIEWS = True
 
 reload_model = False
-num_epochs = 2
+num_epochs = 1
 learning_rate = 0.001
 test_interval = 50
 
 # Loading sataset, use toy = True for obtaining a smaller dataset
-train_dataset, test_dataset, num_words, input_size = ld.get_data_set(BATCH_SIZE, toy=True)
+train_dataset, test_dataset, custom_dataset, num_words, input_size = ld.get_data_set(BATCH_SIZE, toy=SHOW_CUSTOM_REVIEWS)
+print(f'number of training batches: {len(train_dataset)}\n'
+      f'number of test batches: {len(test_dataset)}\n'
+      f'')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Special matrix multipication layer (like torch.Linear but can operate on arbitrary sized
@@ -221,13 +224,23 @@ def select_model(hidden_dimension=HIDDEN_SIZE_LARGE):
 
     return model
 
+def create_save_directories(model_name, hidden_dimension):
+    folder_name = f'{model_name}_{hidden_dimension}'
+    plot_save_directory_path = os.path.join(PLOT_SAVE_DIRECTORY, folder_name)
+    if not os.path.exists(plot_save_directory_path):
+        os.makedirs(plot_save_directory_path)
+    if not os.path.exists(MODEL_SAVE_DIRECTORY):
+        os.makedirs(MODEL_SAVE_DIRECTORY)
+
+    return plot_save_directory_path
+
 def save_accuracy_plot(test_steps, train_accuracies, test_accuracies,
                model_name, hidden_dimension, save_directory_path):
-    title = f'Training and Test Accuracies - {model_name} - {hidden_dimension}'
+    title = f'Training and Test Accuracies - hidden size {hidden_dimension}'
     filename = f'accuracy_plot_{model_name}_{hidden_dimension}.jpeg'
     plot_save_path = os.path.join(save_directory_path, filename)
-
-    plt.subplot(1, 2, 2)
+    print(f'now saving accuracies to {plot_save_path}')
+    plt.figure(figsize=(10, 5))
     plt.plot(train_accuracies, label='Train Accuracy')
     plt.plot([int(s * len(train_accuracies) / num_epochs) for s in test_steps],
              test_accuracies, label='Test Accuracy')
@@ -235,20 +248,21 @@ def save_accuracy_plot(test_steps, train_accuracies, test_accuracies,
     plt.xlabel('Iteration')
     plt.ylabel('Accuracy')
     plt.legend()
-
-    plt.suptitle(f'Model: {model_name}')
     plt.tight_layout()
     plt.savefig(plot_save_path)
     plt.close()
 
 def save_train_test_loss_plot(train_losses, test_losses, test_steps,
                               model_name, hidden_dimension, save_directory_path):
+
     title = f'Training and Test Loss - {model_name} - {hidden_dimension}'
     filename = f'loss_plot_{model_name}_{hidden_dimension}.jpeg'
     plot_save_path = os.path.join(save_directory_path, filename)
+    test_step_indices = [int(e * len(train_losses) / num_epochs) for e in test_steps]
+    print(f'now saving loss plots: {plot_save_path}')
     plt.figure(figsize=(10, 5))
     plt.plot(train_losses, label="Train Loss", alpha=0.6)
-    plt.plot(test_steps, test_losses, label="Test Loss", marker='o', linestyle='--')
+    plt.plot(test_step_indices, test_losses, label="Test Loss", marker='o', linestyle='--')
     plt.xlabel("Training Iteration")
     plt.ylabel("Loss")
     plt.title(title)
@@ -259,11 +273,7 @@ def save_train_test_loss_plot(train_losses, test_losses, test_steps,
 
 def save_plots(train_losses, test_losses, test_steps,
                train_accuracies, test_accuracies,
-               model_name, hidden_dimension):
-    folder_name = f'{model_name}_{hidden_dimension}'
-    save_directory_path = os.path.join(PLOT_SAVE_DIRECTORY, folder_name)
-    if not os.path.exists(save_directory_path):
-        os.makedirs(save_directory_path)
+               model_name, hidden_dimension, save_directory_path):
 
     save_train_test_loss_plot(train_losses, test_losses, test_steps,
                model_name, hidden_dimension, save_directory_path)
@@ -280,7 +290,83 @@ def compute_accuracy(outputs, labels):
     total = labels.size(0)
     return correct / total
 
+def number_to_label(number):
+    output_label = 'positive' if int(number) == 0 else 'negative'
+    return output_label
+
+def tensor_numbers_to_labels(numbers):
+    as_list = numbers.tolist()
+    output = [number_to_label(number) for number in as_list]
+    return output
+
+def make_custom_prediction(best_model, hidden_dimension, save_directory_path):
+    model_name = best_model.name()
+    title = "Model Predictions vs. True Labels"
+    filename = f'Predictions_Custom_Data_{model_name}_{hidden_dimension}.jpeg'
+    plot_save_path = os.path.join(save_directory_path, filename)
+    custom_accuracy_history = []
+    for labels, reviews, reviews_text in custom_dataset:
+        print(f'custom data {labels}\n'
+              f'reviews text {reviews_text}\n')
+        if run_recurrent:  # Recurrent nets (RNN/GRU)
+            hidden_state = best_model.init_hidden(int(labels.shape[0])).to(device)
+
+            for i in range(num_words):
+                output, hidden_state = best_model(reviews[:, i, :], hidden_state)  # HIDE
+
+        else:
+            # Token-wise networks (MLP / MLP + Atten.)
+            sub_score = []
+            if atten_size > 0:
+                sub_score, atten_weights = best_model(reviews)
+            else:
+                sub_score = best_model(reviews)
+            output = torch.mean(sub_score, 1)
+        # cross-entropy loss
+        _, predicted = torch.max(output, 1)
+        _, true_labels = torch.max(labels, 1)
+        predicted_as_strings = tensor_numbers_to_labels(predicted)
+        true_labels_as_strings = tensor_numbers_to_labels(true_labels)
+        correct = (predicted == true_labels)
+        print(f'predicted labels: {predicted}\n'
+              f'true labels: {true_labels}\n'
+              f'correct labels: {correct}\n'
+              f'predicted labels as strings: {predicted_as_strings}\n'
+              f'true labels as strings: {true_labels_as_strings}\n')
+        accuracy = compute_accuracy(output, labels)
+        custom_accuracy_history.append(accuracy)
+        df = pd.DataFrame({
+            'Reviews': list(range(1, len(reviews_text) + 1)),
+            'True Labels': true_labels_as_strings,
+            'Predicted Labels': predicted_as_strings
+        })
+        df['Correct'] = df['True Labels'] == df['Predicted Labels']
+        fig, ax = plt.subplots(figsize=(10, 0.6 * len(df)))
+        ax.axis("off")
+        ax.set_title(title, fontsize=16, weight='bold', pad=20)
+
+        table = ax.table(
+            cellText=df.values,
+            colLabels=df.columns,
+            cellLoc='center',
+            loc='center'
+        )
+        table.auto_set_font_size(False)
+        table.set_fontsize(10)
+
+        for (row, col), cell in table.get_celld().items():
+            if row == 0:
+                cell.set_text_props(weight='bold')
+            if row > 0 and not df.iloc[row - 1]["Correct"]:
+                cell.set_facecolor('#ffdddd')  # light red
+
+        plt.tight_layout()
+
+        plt.savefig(plot_save_path, dpi=300)
+        plt.close()
+
 def train(hidden_dimension=HIDDEN_SIZE_LARGE):
+
     model = select_model(hidden_dimension)
     model.to(device)
 
@@ -298,9 +384,20 @@ def train(hidden_dimension=HIDDEN_SIZE_LARGE):
     train_accuracy_history = []
     test_accuracy_history = []
 
+    plot_save_directory_path = create_save_directories(model.name(), hidden_dimension)
+
+    best_model = None
+    best_test_loss = float('inf')
+    n_train_batches = len(train_dataset)
+    n_test_batches = len(test_dataset)
+
+    train_iterator = iter(train_dataset)
+    test_iterator = iter(test_dataset)
+
     for epoch in range(num_epochs):
 
         itr = 0 # iteration counter within each epoch
+        test_iterations = 0
 
         for labels, reviews, reviews_text in train_dataset:   # getting training batches
 
@@ -311,13 +408,12 @@ def train(hidden_dimension=HIDDEN_SIZE_LARGE):
 
             if (itr + 1) % test_interval == 0:
                 test_iter = True
+                test_iterations += 1
                 labels, reviews, reviews_text = next(iter(test_dataset)) # get a test batch
             else:
                 test_iter = False
 
-            # Recurrent nets (RNN/GRU)
-
-            if run_recurrent:
+            if run_recurrent: # Recurrent nets (RNN/GRU)
                 hidden_state = model.init_hidden(int(labels.shape[0])).to(device)
 
                 for i in range(num_words):
@@ -348,10 +444,12 @@ def train(hidden_dimension=HIDDEN_SIZE_LARGE):
 
             # averaged losses
             if test_iter:
+                print(f'batch {test_iterations} of test set, last review in batch: {reviews_text[-1]}')
                 test_loss = 0.8 * float(loss.detach()) + 0.2 * test_loss
                 test_loss_history.append(test_loss)
                 test_accuracy_history.append(accuracy)
-                test_steps.append(epoch + itr / len(train_dataset))  # fractional epoch
+                x = epoch + itr / len(train_dataset)
+                test_steps.append(x)  # fractional epoch
             else:
                 train_loss = 0.9 * float(loss.detach()) + 0.1 * train_loss
                 train_loss_history.append(train_loss)
@@ -371,12 +469,20 @@ def train(hidden_dimension=HIDDEN_SIZE_LARGE):
                     print_review(reviews_text[0], nump_subs[0,:,0], nump_subs[0,:,1], labels[0,0], labels[0,1])
 
                 # saving the model
-                model_save_path = os.path.join(MODEL_SAVE_DIRECTORY, f'{model.name()}_{hidden_dimension}.pth')
-                torch.save(model, model_save_path)
+                if test_loss < best_test_loss:
+                    best_test_loss = test_loss
+                    best_model = model
+
+    make_custom_prediction(best_model, hidden_dimension, plot_save_directory_path)
+
+    print(f'number of iterations: {itr}')
+    model_save_path = os.path.join(MODEL_SAVE_DIRECTORY, f'{best_model.name()}_{hidden_dimension}.pth')
+    print(f'saving model to {model_save_path}')
+    torch.save(best_model, model_save_path)
 
     save_plots(train_loss_history, test_loss_history, test_steps,
                train_accuracy_history, test_accuracy_history,
-               model.name(), hidden_dimension)
+               best_model.name(), hidden_dimension, plot_save_directory_path)
 
 def experiment_with_custom_review():
     pass
